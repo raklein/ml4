48a307ffeaeb3f52b392895137404822cd91edda
Joe-Hilgard <jhilgard@gmail.com>
2020-05-11 16:32:30 -0500

    Adding a control script _master.R to run all scripts.

---
+++ _master.R
@@ -0,0 +1,7 @@
+source("001_data_cleaning.R")
+source("002_ml4analysis.R")
+source("003_metaviz.R")
+source("004_exp_survey_analysis.R")
+source("005_analyses_supplemental.R")
+knitr::knit("006_ml4_papaja_results.Rmd")
+rmarkdown::render("006_ml4_papaja_results.Rmd", "papaja::apa6_word")
